babcaa8c6687a799f4b41502cf18070d0648d205
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2019-12-17 22:10:30 +0530

    Added function to check inputs for register.
    Added function with return types specifying error code with corresponding descriptions.

--- routes/register.js
+++ routes/register.js
@@ -8,6 +8,29 @@ const hashPassword = function(password) {
   return {hash: crypto.pbkdf2Sync(password, salt, 10000, 512, 'sha512').toString('hex'),salt};
 };
 
+const validRegistration = function(details){
+  if(details.phone.length!=10 || isNaN(Number(details.phone))){
+    //console.log(details.phone.length!=10)
+    //console.log(isNaN(Number(details.phone)))
+    return 1;
+  }
+  //console.log(!validator.isAlphanumeric(details.username));
+  if(!validator.isAlphanumeric(details.username)){
+    return 2;
+  }
+  var pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;;
+  //console.log(/\S/.test(details.password)); will check just whitespace.
+  if(!(details.password.match(pattern))){
+    return 3;
+  }
+  //console.log(!validator.isAlpha(details.name).split(" ").join(""));
+  if(!validator.isAlpha(details.name.split(" ").join(""))){
+    return 4;
+  }
+  console.log("All checks passed, valid user");
+  return 0;
+}
+
 router.post('/', function(req, res) {
   const { body } = req;
   if(!body.username || !body.password || !body.name || !body.phone)
@@ -15,33 +38,27 @@ router.post('/', function(req, res) {
     res.send(400,"Invalid Body");
     return;
   }
-  //console.log(typeof body.phone);
-  if(body.phone.length!=10 || isNaN(Number(body.phone))){
-    //console.log(body.phone.length!=10)
-    //console.log(isNaN(Number(body.phone)))
-    console.log("Invalid phone");
-    res.send(400,"Phone number not valid");
-    return;
+  if(errcode = validRegistration(body)){
+    switch(errcode){
+      case 1: console.log("Invalid phone");
+              res.send(400,"Phone number not valid");
+              return;
+              break;
+      case 2: console.log("Invalid Username");
+              res.send(400,"Invalid Username, letters and numbers only.");
+              return;
+              break;
+      case 3: console.log("Invalid Password");
+              res.send(400,"Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters.");
+              return;
+              break;
+      case 4: console.log("Invalid Name");
+              res.send(400,"Name is supposed to be alphabets");
+              return;
+              break;
+    }
   }
-  //console.log(!validator.isAlphanumeric(body.username));
-  if(!validator.isAlphanumeric(body.username)){
-    console.log("Invalid Username");
-    res.send(400,"Invalid Username, letters and numbers only.");
-    return;
-  }
-  var pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;;
-  //console.log(/\S/.test(body.password)); will check just whitespace.
-  if(!(body.password.match(pattern))){
-    console.log("Invalid Password");
-    res.send(400,"Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters.");
-    return;
-  }
-  //console.log(!validator.isAlpha(body.name).split(" ").join(""));
-  if(!validator.isAlpha(body.name.split(" ").join(""))){
-    console.log("Invalid Name");
-    res.send(400,"Name is supposed to be alphabets");
-    return;
-  }
+  
   let enc =  hashPassword(body.password);
   body.password = enc.hash;
   body.salt = enc.salt;
