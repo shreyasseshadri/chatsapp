dd5a652de58b84d8f4f66d28a3ab2f7b8f7931ac
Piyush Ingale <45006839+PiyushPI07@users.noreply.github.com>
2019-12-18 09:59:47 +0530

    Add files via upload

---
+++ test_interface/message.js
@@ -0,0 +1,68 @@
+var server = "http://"+location.host+"/";
+var ws_server;
+if(location.hostname === "localhost")
+    ws_server = "ws://"+location.host+"/";
+else ws_server = "wss://"+location.host+"/";
+
+var username;
+fetch(server+'self',{
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    credentials: 'include'
+}).then(resp => {return resp.json()})
+.then(json => {
+    document.getElementById('placeholder').innerText += ' '+json.username;
+    username = json.phone;
+});
+
+const ws = new WebSocket(ws_server+'message');
+ws.onerror = (err) => {
+    console.log(err);
+}
+ws.onmessage = (message) => {
+    console.log(message.data);
+};
+let buffer = [];
+function message(){
+
+    
+
+    let msg = document.getElementById('msg').value;
+    let toUser = document.getElementById('to').value;
+    let resp = JSON.stringify({
+            from: username,
+            to: toUser,
+            type: "text",
+            text: msg,
+            timestamp: Date.now()
+        });
+    if(ws){
+        buffer.map((msg)=> {
+            ws.send(msg);
+        })
+        buffer.length = 0;
+        ws.send(resp);
+    }
+    else{
+        console.log("ws not available");
+        buffer.push(resp);
+    }
+}
+
+function history(){
+    let otherUser  = document.getElementById('history').value;
+    fetch(server+'message/history'+'?otherUser='+otherUser,{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+    }).then((resp) => {return resp.json()})
+    .then((json) => {
+        console.log(json);
+    })
+}
