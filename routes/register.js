3ed76dfa9caeba3e62ab8db933246f8828affeae
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2019-12-19 12:20:51 +0530

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -13,23 +13,23 @@ const validRegistration = function(details){
   if(details.phone.length!=10 || isNaN(Number(details.phone))){
     //console.log(details.phone.length!=10)
     //console.log(isNaN(Number(details.phone)))
-    return 1;
+    return [1,"Phone number not valid"]
   }
   //console.log(!validator.isAlphanumeric(details.username));
   if(!validator.isAlphanumeric(details.username)){
-    return 2;
+    return [2,"Invalid Username, letters and numbers only."];
   }
   var pattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;;
   //console.log(/\S/.test(details.password)); will check just whitespace.
   if(!(details.password.match(pattern))){
-    return 3;
+    return [3, "Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters."];
   }
   //console.log(!validator.isAlpha(details.name).split(" ").join(""));
   if(!validator.isAlpha(details.name.split(" ").join(""))){
-    return 4;
+    return [4, "Name is supposed to be alphabets"];
   }
   console.log("All checks passed, valid user");
-  return 0;
+  return [0, "No errors"];
 }
 
 router.post('/', function(req, res) {
@@ -39,26 +39,10 @@ router.post('/', function(req, res) {
     res.send(400,"Invalid Body");
     return;
   }
-  if(errcode = validRegistration(body)){
-    switch(errcode){
-      case 1: console.log("Invalid phone");
-              res.send(400,"Phone number not valid");
-              return;
-              break;
-      case 2: console.log("Invalid Username");
-              res.send(400,"Invalid Username, letters and numbers only.");
-              return;
-              break;
-      case 3: console.log("Invalid Password");
-              res.send(400,"Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters.");
-              return;
-              break;
-      case 4: console.log("Invalid Name");
-              res.send(400,"Name is supposed to be alphabets");
-              return;
-              break;
-    }
-  }
+  
+  const [errCode, errMsg] = validRegistration(body);
+  if(errCode)
+    return res.send(400, errMsg);
   
   let enc =  hashPassword(body.password);
   body.password = enc.hash;
