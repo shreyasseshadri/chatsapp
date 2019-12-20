dcebe9ed43ade168197bbb8b2a8b7359f769ec7d
shreyas <shreyasseshadri@gmail.com>
2019-12-21 00:35:53 +0530

    Message exchange tests added

--- test/init.js
+++ test/init.js
@@ -2,16 +2,37 @@ process.env.NODE_ENV = 'test';
 
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../app').httpServer;
+const url = require('url');
+
 let expect = chai.expect;
+const User = require("../models/user");
 
 chai.use(chaiHttp);
 
-var agent = chai.request.agent(server);
+const dummy_user = {
+    username: "dummy",
+    name: "dummy",
+    password: "DummyUser@1",
+    phone:"5730586835"
+}
+
+var agent;
+let server;
+let app;
 describe('Initial tests',  () => {
-    
-    after(() => {
+    before(async () => {
+        server = require('../app').httpServer;
+        app = require('../bin/www')
+        server.listen(3000)
+        agent = chai.request.agent(server)
+        const resp = await agent.post('/register').send(dummy_user);
+        // console.log(resp)
+        expect(resp).to.have.status(200)
+    })
+
+    after(async () => {
         agent.close();
+        await User.find({'username':dummy_user.username}).remove()
     })
 
     it('responds to /', async () => {
@@ -24,20 +45,22 @@ describe('Initial tests',  () => {
         expect(res).to.have.status(404);
     });
     
-    // it('Direct message access without logging', async () => {
-    //    const res = await agent.get('/message');
-    //    expect(res).to.have.status(401);
-    // })
+    it('Direct message access without logging redirects back to login', async () => {
+       const res = await agent.get('/message');
+       expect(res.redirects).to.be.an('array')
+       expect(res.redirects).to.have.length(1)
+       expect(url.parse(res.redirects[0]).pathname).to.equal("/")
+    })
 
-    // it('Direct message access after logging', async () => {
-    //     const login_res = await agent.post('/auth/login').send({username:'test1',password:'pass1'});
-    //     expect(login_res).to.have.status(200);
+    it('Direct message access after logging', async () => {
+        const login_res = await agent.post('/auth/login').send({username:dummy_user.username,password:dummy_user.password});
+        expect(login_res).to.have.status(200);
 
-    //     //Go to message now
+        //Go to message now
 
-    //     const message_res = await agent.get('/message');
-    //     expect(message_res).to.have.status(200);
+        const message_res = await agent.get('/message');
+        expect(message_res).to.have.status(200);
       
-    // })
+    })
 
 });
--- test/message_test.js
+++ test/message_test.js
@@ -4,6 +4,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var expect = chai.expect;
 
+const User = require("../models/user");
 const WebSocket = require('ws');
 chai.use(chaiHttp);
 
@@ -65,24 +66,51 @@ function get_cookie_from_header(header) {
 let server;
 let agent1;
 let agent2;
+
+const dummy_user1 = {
+    username: "dummy1",
+    name: "dummy",
+    password: "DummyUser@1",
+    phone:"5730586836"
+}
+const dummy_user2 = {
+    username: "dummy2",
+    name: "dummy",
+    password: "DummyUser@2",
+    phone:"5730586837"
+}
+
 describe('Message Tests',  () => {
-    before(() => {
+    before(async () => {
         server = require('../app').httpServer;
         server.listen(3000)
         agent1 = chai.request.agent(server);
-        agent2 = chai.request.agent(server);    
+        agent2 = chai.request.agent(server);
+
+
+        const resp1 = await agent1.post('/register').send(dummy_user1);
+        console.log(resp1.text)
+        expect(resp1).to.have.status(200)
+
+        const resp2 = await agent2.post('/register').send(dummy_user2);
+        console.log(resp2.text)
+        expect(resp2).to.have.status(200)  
+
     })
 
-    after(() => {
+    after(async () => {
         agent1.close();
         agent2.close();
+
+        await User.find({username:dummy_user1.username}).remove()
+        await User.find({username:dummy_user2.username}).remove()
     })
 
-    it('Direct message access after logging', async () => {
-        const login_res1 = await agent1.post('/auth/login').send({username:'test1',password:'pass1'});
+    it('Simple Message exchange', async () => {
+        const login_res1 = await agent1.post('/auth/login').send({username:dummy_user1.username,password:dummy_user1.password});
         expect(login_res1).to.have.status(200);
         
-        const login_res2 = await agent2.post('/auth/login').send({username:'test2',password:'pass2'});
+        const login_res2 = await agent2.post('/auth/login').send({username:dummy_user2.username,password:dummy_user2.password});
         expect(login_res2).to.have.status(200);
 
 
@@ -93,42 +121,30 @@ describe('Message Tests',  () => {
         const client1 = await await_connect('ws://localhost:3000/message',session_cookie1) // This finishes execution only after the Promise given by await_connect is resolved.
         const client2 = await await_connect('ws://localhost:3000/message',session_cookie2)
 
-        await sleep(2000) 
-        client1.send(JSON.stringify({
-            from: 'test1',
-            to: 'test2',
+        await sleep(2000)
+        const msg_sent_by_1 = JSON.stringify({
+            from: dummy_user1.phone,
+            to: dummy_user2.phone,
             type: "text",
             text: 'Hello test2!',
             timestamp: Date.now()
-        }))        
+        })
+        client1.send(msg_sent_by_1)        
 
         var msg1 = await await_listen(client2) // This finishes execution only after the Promise given by await_listen is resolved.
-        expect(msg1.data).to.equal(JSON.stringify({
-            from: 'test1',
-            to: 'test2',
-            type: "text",
-            text: 'Hello test2!',
-            timestamp: Date.now()
-        }))
-        console.log(msg1.data)
+        expect(msg1.data).to.equal(msg_sent_by_1)
 
-        client2.send(JSON.stringify({
-            from: 'test2',
-            to: 'test1',
+        const msg_sent_by_2 = JSON.stringify({
+            from: dummy_user2.phone,
+            to: dummy_user1.phone,
             type: "text",
             text: 'Hello back test1!',
             timestamp: Date.now()
-        }))
+        })
+        client2.send(msg_sent_by_2)
         var msg2 = await await_listen(client1)
 
-        expect(msg2.data).to.equal(JSON.stringify({
-            from: 'test2',
-            to: 'test1',
-            type: "text",
-            text: 'Hello back test1!',
-            timestamp: Date.now()
-        }))
-        console.log(msg2.data)
+        expect(msg2.data).to.equal(msg_sent_by_2)
     })
 
 });
