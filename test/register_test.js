f1fd0a9f4a7e0ec6a0942668f8b37633ea49e354
Feyaz <feyaz.181co119@nitk.edu.in>
2020-01-03 13:20:47 +0530

    Auth test

--- test/register_test.js
+++ test/register_test.js
@@ -30,41 +30,41 @@ describe('Register tests',  () => {
     });
 
     it('Checking username isn\'t whitespace', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'     ',password:'PaSsW0rd!', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'     ',password:'PaSsW0rd!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
     it('Checking username has no spl chars', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'st$@layer',password:'PaSsW0rd!', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'st$@layer',password:'PaSsW0rd!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
 
     it('Checking password length more than 7', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaW0rd!', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaW0rd!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
     it('Checking password length less than 17', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd12345678!', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd12345678!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
     it('Checking password has spl chars', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd1', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd1', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
     it('Checking password has numbers', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsWord!', name:"Gleep glopb"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsWord!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
     });
 
     it('Checking name isn\'t whitespace', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd!', name:" "});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd!', name:" "});
         expect(register_res).to.have.status(400); 
     });
     it('Checking name doesn\'t have spl chars', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd!', name:"Gleep Gl0$b"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd!', name:"Gleep Gl0$b"});
         expect(register_res).to.have.status(400); 
     });
     it('Checking name doesn\'t have spl chars', async () => {
-        const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd!', name:"Gleep Gl0$b"});
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd!', name:"Gleep Gl0$b"});
         expect(register_res).to.have.status(400); 
     });
 });
