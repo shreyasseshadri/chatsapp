7f8ddc5b4e3658cd2ca52737df2134a0d050860e
Feyaz Baker <44121644+NegaMage@users.noreply.github.com>
2020-01-05 00:21:03 +0530

    Tests for register and index (#8)
    * Added phone number check
    
    * Update register.js
    
    * Added function to check inputs for register.
    
    Added function with return types specifying error code with corresponding descriptions.
    
    * Added validator to package.json
    
    Added library required for input validation for registering users.
    
    * Added validator package to register.
    
    * Update register.js
    
    * Made tests for registration page
    
    * Delete register_test.js
    
    * Added registration test
    
    * Index test
    
    * Delete index_test.js
    
    The index_test was intended to check if the person could access /message after a faulty login, but since init already checks that users cannot directly access /message without logging in first, this test is redundant.
    
    * auth test
    
    * Auth test
    
    * Added message verification
    
    * updating file
    
    * Spellfix inavalid to invalid in auth
    
    * Update message_test.js

---
+++ test/auth_test.js
@@ -0,0 +1,58 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const url = require('url');
+
+let expect = chai.expect;
+const User = require("../models/user");
+
+chai.use(chaiHttp);
+
+const dummy_user1 = {
+    username: "dummy1",
+    name: "dummy",
+    password: "DummyUser@1",
+    phone:"5730586836"
+}
+const dummy_user2 = {
+    username: "dummy2",
+    name: "dummy",
+    password: "DummyUser@2",
+    phone:"5730586837"
+}
+
+let server;
+let agent1;
+let agent2;
+
+describe('Auth Test',  () => {
+    before(async () => {
+        server = require('../app').httpServer;
+        server.listen(3000)
+        agent1 = chai.request.agent(server);
+        agent2 = chai.request.agent(server);
+
+        const resp1 = await agent1.post('/register').send(dummy_user1);
+        //console.log(resp1.text)
+        expect(resp1).to.have.status(200)
+    })
+
+    after(async () => {
+        agent1.close();
+        agent2.close();
+
+        await User.find({username:dummy_user1.username}).remove()
+        await User.find({username:dummy_user2.username}).remove()
+    })
+
+    it('Unrecognized users are rejected', async() =>{
+        const login_res1 = await agent1.post('/auth/login').send({username:dummy_user1.username,password:dummy_user1.password});
+        expect(login_res1).to.have.status(200);
+        
+        const login_res2 = await agent2.post('/auth/login').send({username:dummy_user2.username,password:dummy_user2.password});
+        expect(login_res2).to.have.status(401);
+        expect(login_res2).to.have.property('text');
+        expect(login_res2.text).to.equal("Invalid username / Password");
+    });
+});
--- test/register_test.js
+++ test/register_test.js
@@ -8,21 +8,85 @@ let expect = chai.expect;
 
 chai.use(chaiHttp);
 
+const User = require("../models/user");
+
+
 var agent;
+let app;
+
 describe('Register tests',  () => {
     before(async () => {
         server = require('../app').httpServer;
-        server.listen(3000)
-        agent = chai.request.agent(server);
+        app = require('../bin/www')
+        agent = chai.request.agent(server)
     })
 
-    after(() => {
+    after(async () => {
+        
         agent.close();
     })
 
-    it('Checking phone number length', async () => {
+    it('Checking phone number length not less than 10', async () => {
         const register_res = await agent.post('/register').send({phone: '657488907', username:'stlayer',password:'PaSsW0rd!', name:"Gleep glopb"});
         expect(register_res).to.have.status(400); 
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Phone number not valid");
+    });
+    it('Checking phone number length not more than 10', async () => {
+        const register_res = await agent.post('/register').send({phone: '65748890721', username:'stlayer',password:'PaSsW0rd!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400); 
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Phone number not valid");
     });
 
+    it('Checking username isn\'t whitespace', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'     ',password:'PaSsW0rd!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Invalid Username, letters and numbers only."); 
+    });
+    it('Checking username has no spl chars', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'st$@layer',password:'PaSsW0rd!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Invalid Username, letters and numbers only."); 
+    });
+
+    it('Checking password length more than 7', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaW0rd!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters."); 
+    });
+    it('Checking password length less than 17', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd12345678!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters."); 
+    });
+    it('Checking password has spl chars', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd1', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters."); 
+    });
+    it('Checking password has numbers', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsWord!', name:"Gleep glopb"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Password must have 1 uppercase, 1 lowercase, 1 number, 1 special character, and between 8 and 16 characters."); 
+    });
+
+    it('Checking name isn\'t whitespace', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd!', name:" "});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Name is supposed to be alphabets"); 
+    });
+    it('Checking name doesn\'t have spl chars', async () => {
+        const register_res = await agent.post('/register').send({phone: '6574889078', username:'stlayer',password:'PaSsW0rd!', name:"Gleep Gl0$b"});
+        expect(register_res).to.have.status(400);
+        expect(register_res).to.have.property('text');
+        expect(register_res.text).to.equal("Name is supposed to be alphabets"); 
+    });
 });
